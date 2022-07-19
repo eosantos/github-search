198efcf91c259517156f1e230e1f7689ebf865a0
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2022-07-19 20:44:46 -0300

    Feature: Create component alert

---
+++ src/components/layout/Alert.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Alert = () => (
+  <div className="mt-5 mb-5 p-2 bg-red-800 text-gray-100">
+    <h4 className="text-1xl">Digite um login.</h4>
+  </div>
+);
+
+export default Alert;
--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,10 +1,38 @@
-const Search = () => {
+import React, { useState } from "react";
+import Alert from "./Alert";
+
+const Search = ({ searchUser }) => {
+  const [text, setText] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert(true);
+    } else {
+      searchUser(text);
+      setAlert(false);
+    }
+  };
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
       <div className="container mx-auto">
+        {alert && <Alert />}
+
         <h1 className={tailwind.title}>Pesquisar Usuários do GitHub</h1>
-        <form>
-          <input type="text" name="text" className={tailwind.searchInput} />
+        <form onSubmit={handleSumbit}>
+          <input
+            type="text"
+            name="text"
+            className={tailwind.searchInput}
+            onChange={onChangeText}
+            value={text}
+          />
           <input type="submit" value="Pesquisar" className={tailwind.button} />
         </form>
       </div>
